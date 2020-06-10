cff048705e289b8ad95db527aff1c8065ddfd104
Tim Haug <tim.haug@siemens.com>
2020-06-10 11:42:18 +0200

    Initial Commit

---
+++ KeyboardInputDemo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeyboardInputDemo2
+//
+//  Created by Tim Haug on 10.06.20.
+//  Copyright © 2020 timh1004. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KeyboardInputDemo2Tests/KeyboardInputDemo2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  KeyboardInputDemo2Tests.swift
+//  KeyboardInputDemo2Tests
+//
+//  Created by Tim Haug on 10.06.20.
+//  Copyright © 2020 timh1004. All rights reserved.
+//
+
+import XCTest
+@testable import KeyboardInputDemo2
+
+class KeyboardInputDemo2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
