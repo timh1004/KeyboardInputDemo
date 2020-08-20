994e4475bfe69bbb9eeeb4eaf5a3e71d82b5a4f6
Tim Haug <tim.haug@siemens.com>
2020-08-20 14:26:50 +0200

    pressesBegan mit key print

--- KeyboardInputDemo2/ViewController.swift
+++ KeyboardInputDemo2/ViewController.swift
@@ -15,6 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+// https://www.hackingwithswift.com/example-code/uikit/how-to-detect-keyboard-input-using-pressesbegan-and-pressesended
+    
+    override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+//        guard let keys = event?.allPresses.compactMap({ $0.key?.characters }).sorted() else { return }
+//        print("Keys")
+//        for element in keys {
+//          print(element, terminator: " ")
+//        }
+        
+        guard let key = presses.first?.key else { return }
+//        print(key.keyCode, key.characters, key.charactersIgnoringModifiers, key.modifierFlags)
+        print(key)
+        
+        super.pressesBegan(presses, with: event)
+    }
+    
+    override func pressesEnded(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+        guard let key = presses.first?.key else { return }
+//        print(key)
+//        print(key.keyCode, key.characters, key.charactersIgnoringModifiers, key.modifierFlags)
+//        print(key.characters)
+        
+        super.pressesEnded(presses, with: event)
+    }
 
 }
 
